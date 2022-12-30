34c1c02e6249ab27e6080d6b57e8df36901794ac
Fetiska <80163382+Fetiska@users.noreply.github.com>
2022-12-30 13:34:29 +0300

    3 - Perlin Noise

---
+++ #3 Perlin Noise/Plugins/PerlinNoise/Intermediate/Build/Win64/UE4Editor/Inc/Perlin_Noise/Perlin_NoiseBPLibrary.generated.h
@@ -0,0 +1,119 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PERLIN_NOISE_Perlin_NoiseBPLibrary_generated_h
+#error "Perlin_NoiseBPLibrary.generated.h already included, missing '#pragma once' in Perlin_NoiseBPLibrary.h"
+#endif
+#define PERLIN_NOISE_Perlin_NoiseBPLibrary_generated_h
+
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_SPARSE_DATA
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execSetSeed); \
+	DECLARE_FUNCTION(execFourD_Perlin_Fractal); \
+	DECLARE_FUNCTION(execThreeD_Perlin_Fractal); \
+	DECLARE_FUNCTION(execTwoD_Perlin_Fractal); \
+	DECLARE_FUNCTION(execOneD_Perlin_Fractal); \
+	DECLARE_FUNCTION(execFourD_Perlin_Noise); \
+	DECLARE_FUNCTION(execThreeD_Perlin_Noise); \
+	DECLARE_FUNCTION(execTwoD_Perlin_Noise); \
+	DECLARE_FUNCTION(execOneD_Perlin_Noise);
+
+
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execSetSeed); \
+	DECLARE_FUNCTION(execFourD_Perlin_Fractal); \
+	DECLARE_FUNCTION(execThreeD_Perlin_Fractal); \
+	DECLARE_FUNCTION(execTwoD_Perlin_Fractal); \
+	DECLARE_FUNCTION(execOneD_Perlin_Fractal); \
+	DECLARE_FUNCTION(execFourD_Perlin_Noise); \
+	DECLARE_FUNCTION(execThreeD_Perlin_Noise); \
+	DECLARE_FUNCTION(execTwoD_Perlin_Noise); \
+	DECLARE_FUNCTION(execOneD_Perlin_Noise);
+
+
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUPerlin_NoiseBPLibrary(); \
+	friend struct Z_Construct_UClass_UPerlin_NoiseBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UPerlin_NoiseBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/Perlin_Noise"), NO_API) \
+	DECLARE_SERIALIZER(UPerlin_NoiseBPLibrary)
+
+
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_INCLASS \
+private: \
+	static void StaticRegisterNativesUPerlin_NoiseBPLibrary(); \
+	friend struct Z_Construct_UClass_UPerlin_NoiseBPLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UPerlin_NoiseBPLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/Perlin_Noise"), NO_API) \
+	DECLARE_SERIALIZER(UPerlin_NoiseBPLibrary)
+
+
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UPerlin_NoiseBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPerlin_NoiseBPLibrary) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPerlin_NoiseBPLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPerlin_NoiseBPLibrary); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPerlin_NoiseBPLibrary(UPerlin_NoiseBPLibrary&&); \
+	NO_API UPerlin_NoiseBPLibrary(const UPerlin_NoiseBPLibrary&); \
+public:
+
+
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UPerlin_NoiseBPLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPerlin_NoiseBPLibrary(UPerlin_NoiseBPLibrary&&); \
+	NO_API UPerlin_NoiseBPLibrary(const UPerlin_NoiseBPLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPerlin_NoiseBPLibrary); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPerlin_NoiseBPLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPerlin_NoiseBPLibrary)
+
+
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_PRIVATE_PROPERTY_OFFSET
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_31_PROLOG
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_PRIVATE_PROPERTY_OFFSET \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_SPARSE_DATA \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_RPC_WRAPPERS \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_INCLASS \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_PRIVATE_PROPERTY_OFFSET \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_SPARSE_DATA \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_RPC_WRAPPERS_NO_PURE_DECLS \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_INCLASS_NO_PURE_DECLS \
+	MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h_34_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class Perlin_NoiseBPLibrary."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PERLIN_NOISE_API UClass* StaticClass<class UPerlin_NoiseBPLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MyProject_Plugins_PerlinNoise_Source_Perlin_Noise_Public_Perlin_NoiseBPLibrary_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
